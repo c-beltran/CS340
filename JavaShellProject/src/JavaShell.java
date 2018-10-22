289fb7ff13793d8a89ab1971b718274b157f9133
c-beltran <carlosbeltran90@gmail.com>
2018-10-22 01:06:05 -0400

    started java shell project, initiated part 1

--- JavaShellProject/src/JavaShell.java
+++ JavaShellProject/src/JavaShell.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -6,11 +7,12 @@ import java.lang.*;
 import java.util.*;
 
 public class JavaShell {
-
+//	public static String separator = "\\";
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String commandLine;
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		ProcessBuilder pb = new ProcessBuilder();
 		
 		List<String> commands = new ArrayList<String>();
 		
@@ -24,29 +26,33 @@ public class JavaShell {
 		if (commandLine.equals("")) continue;
 		else{
 			//(1) parse the input to obtain the command and any parameters
-			String[] getInput = commandLine.split("\\s+");
-			for(int i=0; i<getInput.length; i++){
-				commands.add(getInput[i]);
-//				System.out.println(commands.get(i));
-			}
-
-			for(int i=0; i<commands.size(); i++){
-				System.out.println(commands.get(i));
+			StringTokenizer getInput = new StringTokenizer(commandLine);
+			//store commands in the List
+			while(getInput.hasMoreTokens()){
+				commands.add(getInput.nextToken());
 			}
 			
-			//create a ProcessBuilder object
-			ProcessBuilder pb = new ProcessBuilder(commands);
+			//need to check for other commands
 			
-			// starting the process 
-	        Process process = pb.start();
-	        
-	        // for reading the output from stream 
-	        BufferedReader stdInput = new BufferedReader(new
-	         InputStreamReader(process.getInputStream())); 
-	        String s = null; 
-	        while ((s = stdInput.readLine()) != null) 
-	        { 
-	            System.out.println(s); 
+			//(2) create a ProcessBuilder object
+			pb = new ProcessBuilder(commands);
+			
+			//(3) starting the process 
+	        try{
+				Process process = pb.start();  //NEED TO FIX TRY CATCH
+		        
+		        //(4) for reading the output from stream 
+		        BufferedReader stdInput = new BufferedReader(new
+		         InputStreamReader(process.getInputStream())); 
+		        String s = null; 
+		        while ((s = stdInput.readLine()) != null) 
+		        { 
+		        //(5) output the contents returned by the command
+		            System.out.println(s); 
+		        }
+	        }
+	        catch (Exception e){
+	        	System.out.println("Please Enter a Valid Input");
 	        }
 		}
 		
