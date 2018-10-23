2627f4f5958f474866418fb5b2e6df09614a3d0d
c-beltran <carlosbeltran90@gmail.com>
2018-10-23 00:34:44 -0400

    implemented part 1,2,3 of the project

--- JavaShellProject/src/JavaShell.java
+++ JavaShellProject/src/JavaShell.java
@@ -11,10 +11,15 @@ public class JavaShell {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String commandLine;
+		Process process;
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 		ProcessBuilder pb = new ProcessBuilder();
+		String homeDir = System.getProperty("user.dir");
+		String cdCommand = null;
+		
 		
 		List<String> commands = new ArrayList<String>();
+		List<String> historyList = new ArrayList<String>();
 		
 		// we break out with <control><C>
 		while (true)
@@ -25,25 +30,80 @@ public class JavaShell {
 		// if the user entered a return, just loop again
 		if (commandLine.equals("")) continue;
 		else{
+			
+			//clear the command list
+			commands = new ArrayList<String>();
+			
+			//store the history commands onto the list
+			historyList.add(commandLine);
+			
+			//history
+			if(commandLine.equals("history")){
+				for(int i=0; i<historyList.size(); i++){
+					System.out.println(i + " " + historyList.get(i));
+				}
+			}
+			else if(commandLine.equals("!!")){
+				if(historyList.size() == 1 ){
+					System.out.println("There are no previous commands. Please continue...");
+					continue;
+				}
+				else{
+					commandLine = historyList.get(historyList.size() - 2);
+//					System.out.println("THIS ARE HISCOMANDS " + commandLine);
+				}
+			}
+			else if(commandLine.matches("[!][0-9]")){
+				String newString = commandLine.replaceAll("\\D+","");
+				int num = Integer.parseInt(newString);
+				commandLine = historyList.get(num);
+				System.out.println(historyList.get(num));
+				
+			}
+			
 			//(1) parse the input to obtain the command and any parameters
 			StringTokenizer getInput = new StringTokenizer(commandLine);
 			//store commands in the List
 			while(getInput.hasMoreTokens()){
+				//add to list
 				commands.add(getInput.nextToken());
 			}
 			
-			//need to check for other commands
 			
-			//(2) create a ProcessBuilder object
-			pb = new ProcessBuilder(commands);
+			//need to check for other commands
+			if(commandLine.equals("cd")){
+				homeDir = System.getProperty("user.dir");
+				System.out.println("Your Current Dir. is: " + homeDir);
+			}
+			else if(commands.get(0).equals("cd") && commands.size() >= 1 ){
+				cdCommand = commands.get(1);
+				homeDir = cdCommand;
+				
+				
+//				System.out.println(homeDir);
+				
+				
+//				System.out.println("DIRRR " + pb.directory(workingDir));
+				
+//				System.out.print("ARE YOU HERE?");
+				
+			}
+			else{
+			
+//			System.out.println("HELLOOOOO "+commands.get(1));
 			
 			//(3) starting the process 
 	        try{
-				Process process = pb.start();  //NEED TO FIX TRY CATCH
-		        
+				//(2) create a ProcessBuilder object
+				pb = new ProcessBuilder(commands);
+	        	//need this here bc we need to know where to cd
+	        	File workingDir = new File(homeDir);
+	        	pb.directory(workingDir);
+	        	
+				process = pb.start();  
+		   
 		        //(4) for reading the output from stream 
-		        BufferedReader stdInput = new BufferedReader(new
-		         InputStreamReader(process.getInputStream())); 
+		        BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream())); 
 		        String s = null; 
 		        while ((s = stdInput.readLine()) != null) 
 		        { 
@@ -52,8 +112,10 @@ public class JavaShell {
 		        }
 	        }
 	        catch (Exception e){
+//	        	e.printStackTrace();
 	        	System.out.println("Please Enter a Valid Input");
 	        }
+		}
 		}
 		
 		/** The steps are:
